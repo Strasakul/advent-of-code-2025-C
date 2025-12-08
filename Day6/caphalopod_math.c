73670b3317763b9605749be1ea95494337ee7ca0
Strasakul <2felix2001@gmx.de>
2025-12-08 14:27:37 +0100

    jesus, i had to rewrite the entire logic, but tbf, this made it simpler (i think)

--- Day6/caphalopod_math.c
+++ Day6/caphalopod_math.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 unsigned long long calculate_problem(char ***matrix, int rows, int problem){
     char operator = matrix[rows-1][problem][0];
@@ -20,6 +21,16 @@ unsigned long long calculate_problem(char ***matrix, int rows, int problem){
         }
     }
     return res;
+}
+
+int is_blank_line(char *str){
+    int len = strlen(str);
+    int count = 0;
+    for(int i = 0; i < len; i++){
+        if(isspace(str[i])) count++;
+    }
+    if(len == count) return 1;
+    return 0;
 }
 
 // Windows strikes again
@@ -81,71 +92,77 @@ int main(int argc, char *argv[])
 
     int rows = 0, cols = 0;
 
-    // get first line for column count
-    if ((read = getLine(&line, &len, fp)) != -1) {
-        rows++;
-        char *tmp = strdup(line);
-        char *tok = strtok(tmp, " \t\n");
-        while (tok) {
-            cols++;
-            tok = strtok(NULL, " \t\n");
-        }
-        free(tmp);
-    }
-    printf("Cols: %d\n", cols);
-
-    // count remaining rows
+    // count rows and cols
     while ((read = getLine(&line, &len, fp)) != -1){
         rows++;
+        if(cols < read) cols = read;
     }
     printf("Rows: %d\n", rows);
+    printf("Cols: %d\n", cols);
 
-    // make matrix
-    char ***matrix = malloc(rows * sizeof(char **));
-    for(int i = 0; i < rows; i++){
-        matrix[i] = malloc(cols * sizeof(char *));
-        for(int j = 0; j < cols; j++){
-            matrix[i][j] = malloc(16); // max length of a token
-        }
+    // make matrix (transposed)
+    char **matrix = malloc(cols * sizeof(char *));
+    for(int i = 0; i < cols; i++){
+        matrix[i] = malloc(rows + 1);
     }
 
-    // fill matrix
+    // fill matrix (transposed)
     rewind(fp);
-    int i = 0;
+    int row = 0;
     while((read = getLine(&line, &len, fp)) != -1){
-
-        char *tmp = strdup(line);
-        char *tok = strtok(tmp, " ");
-        int j = 0;
-        while(tok && j < cols){         // dont count \n col
-            strcpy(matrix[i][j], tok);
-            tok = strtok(NULL, " ");
-            j++;
+        for(int col = 0; col < read && col < cols; col++){
+            matrix[col][row] = line[col];
         }
-        i++;
+        row++;
     }
 
-    // print matrix
-    //for(int r = 0; r < rows; r++){
-    //    for(int c = 0; c < cols; c++){
-    //        printf("%s ", matrix[r][c]);
-    //    }
-    //    printf("\n");
-    //}
+    // null-terminate each row
+    for (int i = 0; i < cols; i++) {
+        matrix[i][rows] = '\0';
+    }
+
+    fclose(fp);
 
     unsigned long long res = 0;
     unsigned long long tmp = 0;
+    int operator = 0;
+
     for(int i = 0; i < cols; ++i){
-        tmp = calculate_problem(matrix, rows, i);
-        printf("tmp = %llu\n", tmp);
-        res += tmp;
+        char *str = matrix[i];
+        if(is_blank_line(str)) {
+            printf("tmp = %llu\n", tmp);
+            res += tmp;
+            tmp = 0;
+            operator = 0;
+            continue;
+        }
+
+        if(operator == 0){
+            operator = matrix[i][rows-1];
+             tmp = atoi(str);
+             continue;
+        }
+        
+        switch(operator){
+            case '+':
+                printf("%llu + %d\n", tmp, atoi(str));
+                tmp += atoi(str);
+                break;
+            case '*':
+                printf("%llu * %d\n", tmp, atoi(str));
+                tmp *= atoi(str);
+                break;
+            default:
+                break;
+        }
     }
+
+    res += tmp;
 
     printf("Total amount = %llu\n", res);
 
     free(line);
     free(matrix);
-    fclose(fp);
 
     return 0;
 }
